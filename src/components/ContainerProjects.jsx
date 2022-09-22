07a3f5aff85199db62e579d039ae1ff219b7a358
MatheusCaires04 <matheus-binI@hotmail.com>
2022-09-22 14:52:22 -0300

    Projeto em andamento

--- src/components/ContainerProjects.jsx
+++ src/components/ContainerProjects.jsx
@@ -5,12 +5,11 @@ import { DataProjects } from "./DataProjects";
 import { BsArrowsFullscreen, BsGithub } from "react-icons/bs";
 
 const ContainerProjects = () => {
-
   return (
     <div className="max-w-[1100px] mx-auto mt-0 md:mt-6 grid grid-col-1 md:grid-cols-2 px-6 md:px-0 gap-4 justify-items-center">
       {DataProjects.map((index) => {
         return (
-          <li key={index.id} className="w-[90%] p-4">
+          <li key={index.id} className="w-full md:w-[90%] p-4">
             <div className="border-[10px] border-orangeLight w-full h-auto relative">
               <div className="h-[20rem]">
                 <Image
@@ -21,18 +20,20 @@ const ContainerProjects = () => {
                 />
               </div>
               <div className="absolute top-0 left-0 w-full h-full bg-black/90 text-white flex flex-col items-center justify-center px-8 py-4 duration-200 opacity-0 hover:opacity-[1]">
-                <h3 className='text-xl font-semibold w-[70%] text-center'>Go to the website or Code on GitHub</h3>
-                <div className='flex gap-4 py-8'>
-                <Link href="/">
-                  <a className="flex items-center gap-2 px-8 py-2 bg-orangeLight rounded-full hover:bg-white hover:text-orangeLight duration-200 font-semibold tracking-wider">
-                    Website <BsArrowsFullscreen />
-                  </a>
-                </Link>
-                <Link href="/">
-                  <a className="flex items-center gap-2 px-8 py-2 bg-orangeLight rounded-full hover:bg-white hover:text-orangeLight duration-200 font-semibold tracking-wider">
-                    GitHub <BsGithub />
-                  </a>
-                </Link>
+                <h3 className="text-xl font-semibold w-[100%]  md:w-[70%] text-center">
+                  Go to the website or Code on GitHub
+                </h3>
+                <div className="flex gap-4 py-8 flex-col md:flex-row">
+                  <Link href="/">
+                    <a className="flex items-center gap-2 px-8 py-2 bg-orangeLight rounded-full hover:bg-white hover:text-orangeLight duration-200 font-semibold tracking-wider">
+                      Website <BsArrowsFullscreen />
+                    </a>
+                  </Link>
+                  <Link href="/">
+                    <a className="flex items-center gap-2 px-8 py-2 bg-orangeLight rounded-full hover:bg-white hover:text-orangeLight duration-200 font-semibold tracking-wider">
+                      GitHub <BsGithub />
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
